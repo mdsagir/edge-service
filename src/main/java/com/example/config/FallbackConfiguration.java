27f06ddfc6271c8436e9b4f70b7de8dea499c1cd
mdsagir <tech.sagir@gmail.com>
2021-06-22 10:14:49 +0530

    edge-service

---
+++ src/main/java/com/example/config/FallbackConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class FallbackConfiguration {
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/catalog-fallback"),
+                        this::handleGetFallback)
+                .andRoute(RequestPredicates.POST("/catalog-fallback"),
+                        this::handlePostFallback);
+    }
+
+    public Mono<ServerResponse> handleGetFallback(ServerRequest request) {
+        return ServerResponse.ok().body(Mono.just(""), String.class);
+    }
+
+    public Mono<ServerResponse> handlePostFallback(ServerRequest request) {
+        return ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+}
